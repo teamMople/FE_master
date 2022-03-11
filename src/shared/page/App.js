363cd8e5a4d748d28080fa41455b911d2a08317b
Maya <suzy.maya.yu@gmail.com>
2022-03-11 15:55:40 +0900

    [Fix] fix merge confilicts

--- src/shared/page/App.js
+++ src/shared/page/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Main, Login, Signup, Welcome } from '../../pages';
@@ -10,60 +8,25 @@ import theme from '../../components/theme';
 import history from '../utils/history';
 import { ThemeProvider } from 'styled-components';
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter history={history}>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/welcome" element={<Welcome />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-=======
-import '../styles/App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CreateChatRoom from '../../chatTestDir/CreateChatRoom';
 import VoiceRoom from '../../chatTestDir/VoiceRoom';
 import ChatRoomList from '../../chatTestDir/ChatRoomList';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
+    <ThemeProvider theme={theme}>
+      <BrowserRouter history={history}>
         <Routes>
+          <Route path="/main" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/welcome" element={<Welcome />} />
           <Route path={'/'} element={<ChatRoomList />} />
           <Route path={'/voice/create'} element={<CreateChatRoom />} />
           <Route path={'/voice/:roomId'} element={<VoiceRoom />} />
         </Routes>
-      </div>
-    </BrowserRouter>
->>>>>>> main
-=======
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import { Main, Login, Signup, Welcome } from '../../pages';
-import '../styles/App.css';
-import theme from '../../components/theme';
-
-import history from '../utils/history';
-import { ThemeProvider } from 'styled-components';
-
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter history={history}>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/welcome" element={<Welcome />} />
-        </Routes>
       </BrowserRouter>
     </ThemeProvider>
->>>>>>> f447579d82aef4f7284c45ca951190a1bf95435e
   );
 }
 

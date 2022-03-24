c6c1265bfe48830c2979b717255590b438c978a3
Kim-DongSeob <imjambo0916@gmail.com>
2022-03-24 23:07:37 +0900

    refactor : refactor chat api request code
    음성 채팅 관련 api 호출 함수 redux 파일에서 관리

--- src/pages/chats/CreateRoom.js
+++ src/pages/chats/CreateRoom.js
@@ -19,21 +19,20 @@ const CreateRoom = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const roomState = useSelector(selectRoomState);
   const selectMenu = [{ value: '공개토론' }, { value: '비공개토론' }];
   const options = [
     { value: '일상생활', label: '일생생활' },
     { value: '직장생활', label: '직장생활' },
     { value: '학교생활', label: '학교생활' },
-    { value: '시사/이슈', label: '시사/이슈' },
-    { value: '관계/심리', label: '관계/심리' },
+    { value: '시사이슈', label: '시사/이슈' },
+    { value: '관계심리', label: '관계/심리' },
     { value: '기타', label: '기타' },
   ];
 
   const createRoom = async () => {
     const data = {
       roomName: roomName,
-      category: selectedOption,
+      category: selectedOption.value,
       moderator: moderator,
       maxParticipantCount: memberCount,
       content: content,
@@ -53,55 +52,6 @@ const CreateRoom = () => {
         );
       })
       .catch((err) => console.error(err));
-
-    // 임시 : header 에 토큰값 넘길 것
-    // const headers = {
-    //   headers: {
-    //     Authorization: `Bearer ${token}`,
-    //   },
-    // };
-    // await axios
-    //   .post(
-    //     `${process.env.REACT_APP_API_URL}/auth/api/chat/room`,
-    //     data,
-    //     headers,
-    //   )
-    //
-    //   // 이거는 방을 만들 때!!!!!!
-    //   .then(async (res) => {
-    //     console.log(res.data.roomId);
-    //     await setRoomName(res.data.roomName);
-    //     // navigateVoiceRoom(roomId, roomName);
-    //     const status = {
-    //       role: 'MODERATOR',
-    //       roomId: res.data.roomId,
-    //       roomName: res.data.roomName,
-    //       category: res.data.category,
-    //       moderatorId: res.data.moderatorId,
-    //       moderatorNickname: res.data.moderatorNickname,
-    //       maxParticipantCount: res.data.maxParticipantCount,
-    //       content: res.data.content,
-    //       isPrivate: res.data.isPrivate,
-    //       agreeCount: res.data.agreeCount,
-    //       disagreeCount: res.data.disagreeCount,
-    //       onAir: res.data.onAir,
-    //       createdAt: res.data.createdAt,
-    //       memberAgreed: res.data.memberAgreed,
-    //       memberDisagreed: res.data.memberDisagreed,
-    //       memberName: 'a',
-    //       accessToken: token,
-    //     };
-    //     dispatch(setJoinRoomStatus(status));
-    //     await navigateVoiceRoom(
-    //       res.data.roomId,
-    //       res.data.roomName,
-    //       res.data.maxParticipantCount,
-    //     );
-    //     //
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
   };
 
   const navigateVoiceRoom = async (
@@ -116,12 +66,6 @@ const CreateRoom = () => {
       role: role,
       participantCount: maxParticipantCount,
     };
-    // const headers = {
-    //   headers: {
-    //     Authorization: `Bearer ${token}`,
-    //   },
-    // };
-    console.log('navigateVoiceRoom :: ', data);
     await dispatch(
       joinRoomAsync({
         data,

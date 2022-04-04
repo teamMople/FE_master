2e57e8fbe1f488f1b9917a2dc4cc3c54b8eb96e1
Maya <suzy.maya.yu@gmail.com>
2022-04-04 20:33:05 +0900

    fix : public/index.html에 fcm script 추가

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,9 +3,19 @@ importScripts(
   'https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js',
 );
 
-firebase.initializeApp({
+const config = {
+  apiKey: 'AIzaSyD0u9HX41rjh3MnO93isinkSuxzLEH22GI',
+  authDomain: 'boiler-e3497.firebaseapp.com',
+  projectId: 'boiler-e3497',
+  storageBucket: 'boiler-e3497.appspot.com',
   messagingSenderId: '128639882477',
-});
+  appId: '1:128639882477:web:4e0c086f572ce6b9a468e4',
+  measurementId: 'G-V83CYYD10V',
+};
+
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
 
 messaging.setBackgroundMessageHandler(function (payload) {
   const title = 'boilerPlate';

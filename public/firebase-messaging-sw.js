d232f096f926158bbf239549c48eddcea1bc0cf7
Maya <suzy.maya.yu@gmail.com>
2022-04-04 20:59:05 +0900

    fix : fcm service worker 수정

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,24 +3,4 @@ importScripts(
   'https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js',
 );
 
-const config = {
-  apiKey: 'AIzaSyD0u9HX41rjh3MnO93isinkSuxzLEH22GI',
-  authDomain: 'boiler-e3497.firebaseapp.com',
-  projectId: 'boiler-e3497',
-  storageBucket: 'boiler-e3497.appspot.com',
-  messagingSenderId: '128639882477',
-  appId: '1:128639882477:web:4e0c086f572ce6b9a468e4',
-  measurementId: 'G-V83CYYD10V',
-};
-
-firebase.initializeApp(config);
-
-const messaging = firebase.messaging();
-
-messaging.setBackgroundMessageHandler(function (payload) {
-  const title = 'boilerPlate';
-  const options = {
-    body: payload.data.status,
-  };
-  return self.registration.showNotification(title, options);
-});
+firebase.initializeApp({ messagingSenderId: '128639882477' });

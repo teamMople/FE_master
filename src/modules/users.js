93ab33c30a4b7a67cf142e738b9db382a30f23e8
Maya <suzy.maya.yu@gmail.com>
2022-04-05 20:59:53 +0900

    fix : kakao login 수정

--- src/modules/users.js
+++ src/modules/users.js
@@ -86,10 +86,7 @@ export const kakaoLoginAsync = createAsyncThunk(
       .then((response) => {
         console.log(response);
         if (response.status === 200) {
-          // setCookie('token', response.data.Authorization, 1);
           localStorage.setItem('token', response.data.Authorization);
-          setCookie('token', localStorage.getItem('token'), 1);
-          localStorage.removeItem('token');
           localStorage.setItem('userId', response.data.userId);
           localStorage.setItem('email', response.data.email);
           localStorage.setItem('nickname', response.data.nickname);
@@ -237,7 +234,11 @@ export const userSlice = createSlice({
     logout: (state) => {
       state.isLogin = false;
       deleteCookie('token');
-      localStorage.removeItem('loginUser');
+      localStorage.removeItem('token');
+      localStorage.removeItem('userId');
+      localStorage.removeItem('email');
+      localStorage.removeItem('nickname');
+      localStorage.removeItem('profileImageUrl');
       window.location.assign('/');
     },
   },
